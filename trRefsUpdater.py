883be701ea68f0a75a71172870b024c481861d87
Vahan Sosoyan <sosoyan@gmail.com>
2016-09-08 20:01:03 +0400

    Update trRefsUpdater.py

--- trRefsUpdater.py
+++ trRefsUpdater.py
@@ -1,19 +1,38 @@
 """
-This module helps to iterate through 
+This module helps to iterate through
 all referenced files in the current maya scene,
 check for updates and replace with the new version.
 """
 
 __author__ = "Vahan Sosoyan"
-__copyright__ = "Copyright 2014, Triada Studio"
+__copyright__ = "Copyright 2016"
 __license__ = "GPL"
-__version__ = "1.6.1"
+__version__ = "2.0"
 
 import maya.cmds as cmds
-import os, glob, sys
+import maya.utils as utils
+import os, glob, sys, re
 import maya.OpenMaya as api
 
-def refCheckUpdateDialog(newVersionsNames,newVersionsList):
+class refNode(object):
+    def __init__(self, name, path, isLoaded):
+        self.name = name
+        self.path = path
+        self.isLoaded = isLoaded
+        self.fileName = os.path.splitext(os.path.basename(path))[0]
+        self.version = re.search(r".v\d+", self.fileName).group()[2:]
+
+        self.updatePath = glob.glob(self.path.replace(self.version, "*"))[-1]
+        self.updateFileName = os.path.splitext(os.path.basename(self.updatePath))[0]
+        self.updateVersion = re.search(r".v\d+", self.updateFileName).group()[2:]
+
+        self.hasUpdate = (self.updatePath != self.path)
+
+    def update(self):
+        if self.isLoaded and self.hasUpdate:
+            cmds.file(self.updatePath, lr=self.name, lrd="topOnly")
+
+def refCheckUpdateDialog(newVersionsNames, newVersionsList):
 
     newVersionsCount = len(newVersionsList)
     refAssetsString = ""
@@ -30,162 +49,80 @@ def refCheckUpdateDialog(newVersionsNames,newVersionsList):
     for i in newVersionsNumsList:
         refAssetsString += "%s\n"%i
 
-    return cmds.confirmDialog(title="TRIADA Check for Updates",
-                              message="%s new versions of asset references has been found\n\n%s\n%s"
+    return cmds.confirmDialog(title="TRIADA Check for Updates v2.0",
+                              message="%s new versions of asset references have been found\n\n%s\n%s"
                               %(newVersionsCount,refAssetsString,note),
-                              button=["Update by one","Update All","Skip by one", "Cancel"],
+                              button=["Update All", "Update by One","Skip by One", "Cancel"],
                               defaultButton="Update",
                               cancelButton="Cancel",
                               dismissString="Cancel",
                               icn="information")
 
-def refsUpdateChecker(ifLoaded,topRef):
-    def uniq(input):
-        output = []
-        for x in input:
-            if x not in output:
-                output.append(x)
-        return output
+def getRefNodesList():
+    # Get reference nodes
+    refSel = [i for i in cmds.ls(rf=1) if "_UNKNOWN_REF_NODE_" not in i]
 
-    def compare(src, test):
-        if len(src) != len(test):
-            return
-        diffmap = [0]*len(src)
-        count = 0
-        for i, c in enumerate(src):
-            if not c == test[i]:
-                count = count+1
-                diffmap[i] = 1
-        return diffmap
-
-    refSel = cmds.ls(rf=1)
-
+    # Remove ghost ref nodes from selection
     for i in refSel:
-        if "_UNKNOWN_REF_NODE_" in i:
+        try:
+            cmds.referenceQuery(i, il=1)
+        except:
             refSel.remove(i)
-    
-    if ifLoaded==1 and topRef==1:
-        refLoaded = []
-        for i in refSel:
-            refLoaded.append(cmds.referenceQuery(i, il=1))
-        refListLoaded = [d for (d, remove) in zip(refSel, refLoaded) if remove]  
-        refListDup=[]
-        for i in refListLoaded:
-            refListDup.append(cmds.referenceQuery(i, rfn=1, tr=1))
-        refList = uniq(refListDup)
-    elif ifLoaded==1 and topRef==0:   
-        refLoaded = []
-        for i in refSel:
-            refLoaded.append(cmds.referenceQuery(i, il=1))
-        refListLoaded = [d for (d, remove) in zip(refSel, refLoaded) if remove]
-        refList = refListLoaded
-    elif ifLoaded==0 and topRef==1:
-        refListDup=[]
-        for i in refSel:
-            refListDup.append(cmds.referenceQuery(i, rfn=1, tr=1))
-        refList = uniq(refListDup)
-    else:
-        refList = refSel
 
-    refFilePaths = []
+    # Create refNodes objects list
+    refNodes = []
+    for i in refSel:
+        name = cmds.referenceQuery(i, rfn=1)
+        path = cmds.referenceQuery(i, f=1, wcn=1)
+        isLoaded = cmds.referenceQuery(i, il=1)
 
-    for i in refList:
-        refFilePaths.append(cmds.referenceQuery(i, f=1, wcn=1))
+        # Append only versioned ref nodes
+        fileName = os.path.splitext(os.path.basename(path))[0]
+        if re.search(r".v\d+", fileName) != None:
+            refNodes.append(refNode(name, path, isLoaded))
+    return refNodes
 
-    refFileVersionList = []
+def refsUpdater(*args):
+    nodeList = getRefNodesList()
+    nodeUpdateList = [i for i in nodeList if (i.hasUpdate and i.isLoaded)]
+    updatedList, skippedList = [], []
 
-    for i in refFilePaths:
-        refFileDirList = os.path.abspath(os.path.join(i, os.pardir))
-        refFileList = os.path.splitext(os.path.basename(i))[0]
-        refFileExt = os.path.splitext(os.path.basename(i))[1]
-        refFileName = refFileList[:refFileList.rfind("_v")+2]+"*%s"%refFileExt
-        refFileVersionList.append(glob.glob("%s/%s*"%(refFileDirList,refFileName)))
+    def updater():
+        if len(nodeUpdateList) > 0:
+            nameList = [i.name for i in nodeUpdateList if (i.hasUpdate and i.isLoaded)]
+            updatePathList = [i.updatePath for i in nodeUpdateList if (i.hasUpdate and i.isLoaded)]
+            dialog = refCheckUpdateDialog(nameList, updatePathList)
 
-    refFileNewVersionPaths=[]
-    
-    for i in refFileVersionList:
-        refFileNewVersionPaths.append(i[-1])
+            if dialog == "Update All":
+                for i in nodeUpdateList:
+                    i.update()
+                    updatedList.append(i.name)
 
-    refFileFullName = []
-    refFileNewVersionFullName = []
+            elif dialog == "Update by One":
+                nodeUpdateList[0].update()
+                updatedList.append(nodeUpdateList[0].name)
+                del nodeUpdateList[0]
+                updater()
 
-    for i in refFilePaths:
-        refFileFullName.append(os.path.basename(i))
-
-    for i in refFileNewVersionPaths:
-        refFileNewVersionFullName.append(os.path.basename(i))
-
-    indexList = compare(refFileNewVersionFullName,refFileFullName)
-    indices = [i for i, x in enumerate(indexList) if x == 1]
-
-    refListDif  = []
-    refFileNewVersionPathsDif=[]
-
-    for i in indices:
-        refListDif.append(refList[i])
-        refFileNewVersionPathsDif.append(refFileNewVersionPaths[i])
-
-    return list([refListDif, refFileNewVersionPathsDif, refList])
-
-def refsUpdater(**kwargs):
-    ifLoaded = kwargs.setdefault("il",1)
-    topRef = kwargs.setdefault("tr",1)
-    callBack = kwargs.setdefault("cb",0)
-    loadRefDepth = kwargs.setdefault("lrd","topOnly")
-    refsUpdateCheckerOutput = refsUpdateChecker(ifLoaded,topRef)
-    updateList = []
-    skipList = []
-    def mainRefsUpdater(callBack):
-        if len(refsUpdateCheckerOutput[2]) > 0:
-            if len(refsUpdateCheckerOutput[1]) > 0:
-                refCheckUpdateDialogAnswer = refCheckUpdateDialog(refsUpdateCheckerOutput[0],refsUpdateCheckerOutput[1])
-                if refCheckUpdateDialogAnswer == "Update by one":
-                    cmds.file(refsUpdateCheckerOutput[1][0],lr=refsUpdateCheckerOutput[0][0],lrd=loadRefDepth)
-                    updateList.append(refsUpdateCheckerOutput[0][0])
-                    del refsUpdateCheckerOutput[0][0]
-                    del refsUpdateCheckerOutput[1][0]
-                    mainRefsUpdater(callBack)
-                elif refCheckUpdateDialogAnswer == "Update All":
-                    for i in refsUpdateCheckerOutput[0]:
-                        updateList.append(i)
-                    for i,k in zip(refsUpdateCheckerOutput[0],refsUpdateCheckerOutput[1]):
-                        cmds.file(k,lr=i,lrd=loadRefDepth)
-                    if len(skipList) == 0:
-                        sys.stdout.write("// Info: %s updated."%(", ".join(updateList)))
-                    else:
-                        sys.stdout.write("// Info: %s updated | %s skipped."
-                        %((", ".join(updateList)),(", ".join(skipList))))
-                elif refCheckUpdateDialogAnswer == "Skip by one":
-                    skipList.append(refsUpdateCheckerOutput[0][0])
-                    del refsUpdateCheckerOutput[0][0]
-                    del refsUpdateCheckerOutput[1][0]
-                    mainRefsUpdater(callBack)
-                elif refCheckUpdateDialogAnswer == "Cancel":
-                    for i in refsUpdateCheckerOutput[0]:
-                        skipList.append(i)
-                    if len(updateList) == 0:
-                        sys.stdout.write("// Info: %s skipped."%(", ".join(skipList)))
-                    else:
-                        sys.stdout.write("// Info: %s updated | %s skipped."
-                        %((", ".join(updateList)),(", ".join(skipList))))
-            else:
-                if len(updateList)>0 or len(skipList)>0:
-                    if len(updateList) == 0:
-                            sys.stdout.write("// Info: %s skipped."%(", ".join(skipList)))
-                    elif len(skipList) == 0:
-                            sys.stdout.write("// Info: %s updated."%(", ".join(updateList)))
-                    else:
-                        sys.stdout.write("// Info: %s updated | %s skipped."
-                        %((", ".join(updateList)),(", ".join(skipList))))
-                else:
-                    sys.stdout.write("// Info: All assets are up to date.")
+            elif dialog == "Skip by One":
+                skippedList.append(nodeUpdateList[0].name)
+                del nodeUpdateList[0]
+                updater()
         else:
-            if callBack == 0:
-                cmds.warning("There are no any referenced assets in the current scene.")
-    mainRefsUpdater(callBack)
+            sys.stdout.write("// Info: All assets are up to date.\n")
+    updater()
 
-def refsUpdaterExe(*args):
-    refsUpdater(il=1,tr=1,lrd="topOnly",cb=1)
+    # Unload if needed
+    for i in nodeList:
+        loaded = cmds.referenceQuery(i.name, il=1)
+        if not loaded and i.isLoaded:
+            cmds.file(i.path, loadReference=i.name)
 
-def refsUpdaterCallback():
-    api.MSceneMessage.addCallback(api.MSceneMessage.kAfterOpen, refsUpdaterExe)
+    # Status Message
+    if len(updatedList) > 0:
+        sys.stdout.write("// Info: %s updated.\n"%(", ".join(updatedList)))
+    if len(skippedList) > 0:
+        sys.stdout.write("// Info: %s skipped.\n"%(", ".join(skippedList)))
+
+if __name__ != "__main__":
+    api.MSceneMessage.addCallback(api.MSceneMessage.kAfterOpen,  refsUpdater)
